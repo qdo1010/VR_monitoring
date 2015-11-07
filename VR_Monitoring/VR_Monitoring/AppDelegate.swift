3958522c1b4800770a1d60ba61e7ea229eecc7f5
Sarada Symonds <symonds.sarada@gmail.com>
2015-11-07 11:42:13 -0500

    xcode w parse sdk

--- VR_Monitoring/VR_Monitoring/AppDelegate.swift
+++ VR_Monitoring/VR_Monitoring/AppDelegate.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("ExsqhX6lL8RlwcMZuNPHsKEV2Tb17fFz5WpnLaRN",
+            clientKey: "IXax5T864A79LNKweHrq7ED361EFJz0YVecKNI2d")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+
         return true
     }
 
